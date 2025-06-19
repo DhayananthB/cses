d24c26f0acfcc798138fd6210a9f3b73ca086120
Dhayananth B <dhayananthtn@gmail.com>
2025-06-19 12:35:26 +0530

    added coin combinations ii

--- src/coinCombinationsII.java
+++ src/coinCombinationsII.java
@@ -3,8 +3,6 @@ import java.util.Scanner;
 public class coinCombinationsII {
     final static int MOD = 1_000_000_007;
 
-
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -13,7 +11,16 @@ public class coinCombinationsII {
         for (int i = 0; i < n; i++) {
             coins[i] = sc.nextInt();
         }
-  
+        int[] dp = new int[sum + 1];
+        dp[0] = 1;
+        for (int c : coins) {
+            for (int i = c; i <= sum; i++) {
+                if (i - c >= 0) {
+                    dp[i] = (dp[i] + dp[i - c]) % MOD;
+                }
+            }
+        }
+        System.out.println(dp[sum]);
 
         sc.close();
     }
